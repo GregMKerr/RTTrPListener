955a20a4f9076d1430d8f813591b897af67a427b
Greg <greg.m.kerr@gmail.com>
2014-03-19 20:01:08 -0400

    initial listener setup
    updated gitignore
    
    added deleted file

---
+++ RTTrPListener/RTTrPListener.cpp
@@ -0,0 +1,55 @@
+#include <boost\asio.hpp>
+#include <boost\bind.hpp>
+#include <boost\array.hpp>
+
+using namespace boost::asio::ip;
+
+
+int main(int argc, char* argv[])
+{
+	boost::asio::io_service service;
+	Receiver rec = Receiver(service);
+
+	service.run();
+
+	while(1){}
+
+	return 0;
+}
+
+class Receiver{
+public:
+	Receiver(boost::asio::io_service& io_service)
+		: m_socket(io_service)
+	{
+
+		boost::asio::ip::address ipAddr = boost::asio::ip::address_v4::any();
+		boost::asio::ip::udp::endpoint listen_endpoint(ipAddr, 23001);
+
+		m_socket.open(listen_endpoint.protocol());
+		m_socket.bind(listen_endpoint);
+
+		m_socket.async_receive_from(
+			boost::asio::buffer(m_data), m_sender_endpoint,
+			boost::bind(&Receiver::handle_receive_from, this, boost::asio::placeholders::error, 
+				boost::asio::placeholders::bytes_transferred));
+	}
+
+	void handle_receive_from(const boost::system::error_code& error,
+	  size_t bytes_recvd)
+	{
+	std::cout << "receive" << bytes_recvd << std::endl;
+
+
+	  m_socket.async_receive_from(
+		  boost::asio::buffer(m_data), m_sender_endpoint,
+		  boost::bind(&Receiver::handle_receive_from, this,
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred));
+	}
+
+private:
+	udp::socket m_socket;
+	udp::endpoint m_sender_endpoint;
+	boost::array<char, 100> m_data;
+};

dda95a5f9f5a2e736a3da73154037b4f815ae7d0
Greg Kerr <greg.m.kerr@gmail.com>
2014-03-20 18:45:08 -0400

    switched from broadcast to multicast, corrected listening port

--- RTTrPListener/RTTrPListener.cpp
+++ RTTrPListener/RTTrPListener.cpp
@@ -5,28 +5,30 @@
 
 using namespace boost::asio::ip;
 
-class Receiver;
+const int multicast_port = 24002;
 
 class Receiver{
 public:
-	Receiver(boost::asio::io_service& io_service) : m_socket(io_service)
+	//io_service, listen_address, multicast_address
+	Receiver(boost::asio::io_service& io_service, const address& listen_address, const address& multicast_address)
+		: socket(io_service), listen_address(listen_address), multicast_address(multicast_address)
 	{
 	}
 
 	int init(){
-		boost::asio::ip::address ipAddr = boost::asio::ip::address_v4::any();//from_string("238.210.10.1")
-		boost::asio::ip::udp::endpoint listen_endpoint(ipAddr, 59261);
-
-		std::cout << listen_endpoint << std::endl;
-
 		try{
-		m_socket.open(listen_endpoint.protocol());
-		m_socket.bind(listen_endpoint);
 
-		m_socket.async_receive_from(
-			boost::asio::buffer(m_data), m_sender_endpoint,
-			boost::bind(&Receiver::handle_receive_from, this, boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred));
+			udp::endpoint listen_endpoint(listen_address, multicast_port);
+			socket.open(listen_endpoint.protocol());
+			socket.set_option(udp::socket::reuse_address(true));
+			socket.bind(listen_endpoint);
+
+			socket.set_option(multicast::join_group(multicast_address));
+
+			socket.async_receive_from(
+				boost::asio::buffer(data, max_length), sender_endpoint,
+				boost::bind(&Receiver::handle_receive_from, this, boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
 		}
 		catch (std::exception& e)
 		{
@@ -36,57 +38,36 @@ public:
 		return 0;
 	}
 
-private:
 	void handle_receive_from(const boost::system::error_code& error,
 	  size_t bytes_recvd)
 	{
-	std::cout << "receive" << bytes_recvd << std::endl;
+		std::cout << "receive" << bytes_recvd << std::endl;
 
-
-	  m_socket.async_receive_from(
-		  boost::asio::buffer(m_data), m_sender_endpoint,
-		  boost::bind(&Receiver::handle_receive_from, this,
-			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred));
+		socket.async_receive_from(
+			boost::asio::buffer(data), sender_endpoint,
+			boost::bind(&Receiver::handle_receive_from, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
 	}
-
-	udp::socket m_socket;
-	udp::endpoint m_sender_endpoint;
-	boost::array<char, 100> m_data;
+	
+private:
+	udp::socket socket;
+	udp::endpoint sender_endpoint;
+	const address& listen_address;
+	const address& multicast_address;
+	enum { max_length = 1024};
+	boost::array<char, max_length> data;
 };
 
 
 int main(int argc, char* argv[])
 {
 	boost::asio::io_service service;
-	Receiver rec(service);
+	Receiver rec(service, address::from_string("0.0.0.0"), address::from_string("238.210.10.1"));
 	if (rec.init() != 0) { return 1; }
 	service.run();
 
 	while(1){}
 
 	return 0;
-
-	//try
-	//{
-	//	boost::asio::io_service io_service;
-
-	//	udp::socket socket(io_service, udp::endpoint(udp::v4(), 59261));
-
-	//	udp::endpoint sender_endpoint;
-
-	//	// Receive data.
-	//	boost::array<char, 4> buffer;
-	//	std::size_t bytes_transferred = socket.receive_from(boost::asio::buffer(buffer), sender_endpoint);
-
-	//	std::cout << "got " << bytes_transferred << " bytes." << std::endl;
-	//	
-	//}
-	//catch (std::exception& e)
-	//{
-	//	std::cout << "Standard exception: " << e.what() << std::endl;
-	//}
-
-	//int test;
-	//std::cin >> test;
 }
